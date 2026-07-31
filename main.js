19b40f9406eedcb87f17dab0114459c4045ab95c
Ayraxyn <cyvellyeous@gmail.com>
2026-07-31 10:02:38 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ const darkWarning = document.querySelector("#darkModeWarning");
 const warningTrans = document.querySelector("#warningTransform");
 const scoreBox = document.querySelector("#scoreBox");
 const finishButton = document.querySelector("#finishButton");
-const gameInstruct = document.querySelector("#gameInstructions");
 const cursorClick = document.querySelector("#stupidRedCircle");
 const Timer = document.querySelector("#Timer");
 const playMusic = new Audio("audio/gameTheme.mp3"); //credit to v/s
@@ -47,6 +46,8 @@ let feltPageOn = 1; // 123, defaults 1
 let submittedEmail = false;
 let hasDoneQuiz = false;
 let scoreofQuiz = 0;
+let refillableWoolID = null;
+let elapsedFrames = 0;
 //animation1 in home image
 const images = [ // put homeImage1, homeImage2 etc all here
 document.querySelector("#imageA"),
@@ -92,7 +93,6 @@ let currentMinigameTheme = 0; // 0 = day, 1 = night
 let numberOfWarningClicks = 0;
 // used to calc distance later, take note of the coords (i added top 0 left 0 from start
 // for easier calculation)
-let gapWoolCoordX = 850;
 let gapWoolCoordY1 = 10;
 let gapWoolCoordY2 = 160;
 let gapWoolCoordY3 = 310;
@@ -132,7 +132,7 @@ function startGame() {
     toggleBlackCover();
     brightMode.style.display = "none";
     darkMode.style.display = "none";
-    spritePlay.style.display = "none"
+    spritePlay.style.display = "none";
     scoreBox.style.display = "block";
     finishButton.style.display = "block";
     Timer.style.display = "block";
@@ -152,8 +152,8 @@ if (windowWidth > 800) {
 document.querySelector("#darkwoolCreate" +i).style.left = "850px";
 document.querySelector("#whitewoolCreate" +i).style.left = "850px";
 } else {
-    document.querySelector("#darkwoolCreate" +i).style.left = "250px";
-document.querySelector("#whitewoolCreate" +i).style.left = "250px";
+    document.querySelector("#darkwoolCreate" +i).style.left = "150px";
+document.querySelector("#whitewoolCreate" +i).style.left = "150px";
 }
 }
    // create random wools
@@ -211,9 +211,7 @@ newWool.classList.add("isBlackOutlineWool");
   }
 playArea.appendChild(newWool);
 }
-function changeBackgroundTheme() {
 
-}
 
 function createWoolSocket(idnumber) {
 let darkwoolID = "darkwoolCreate" + idnumber;
@@ -243,7 +241,7 @@ break;
 }
 document.querySelector("#" + whitewoolID).addEventListener("click", function () { // click off
 socketCursorCheck(hueType, whitewoolID); // whiteWoolCreate0 etc
-})
+});
 }
 function socketCursorCheck(hue, socketID) {
     if (window.getComputedStyle(Timer).display === "block") { // if stupid red dot is visible means a wool ahs been checked
@@ -265,42 +263,42 @@ switch (true) {
 case cursorClick.classList.contains("isYellowWool"):
    if (document.querySelector("#refillWoolCreate" + (currentWoolGrabbed - 1)).classList.contains("isYellowWool")) {
     if (document.querySelector("#" +socketID).classList.contains("isYellowWool")) {
-++storePoint
+++storePoint;
     }
    }
 break;
 case cursorClick.classList.contains("isGreenWool"):
      if (document.querySelector("#refillWoolCreate" + (currentWoolGrabbed - 1)).classList.contains("isGreenWool")) {
        if (document.querySelector("#" +socketID).classList.contains("isGreenWool")) {
-++storePoint
+++storePoint;
     }
    }
 break;
 case cursorClick.classList.contains("isBlueWool"):
      if (document.querySelector("#refillWoolCreate" + (currentWoolGrabbed - 1)).classList.contains("isBlueWool")) {
        if (document.querySelector("#" +socketID).classList.contains("isBlueWool")) {
-++storePoint
+++storePoint;
     }
    }
 break;
 case cursorClick.classList.contains("isPurpleWool"):
      if (document.querySelector("#refillWoolCreate" + (currentWoolGrabbed - 1)).classList.contains("isPurpleWool")) {
        if (document.querySelector("#" +socketID).classList.contains("isPurpleWool")) {
-++storePoint
+++storePoint;
     }
    }
 break;
 case cursorClick.classList.contains("isPinkWool"):
      if (document.querySelector("#refillWoolCreate" + (currentWoolGrabbed - 1)).classList.contains("isPinkWool")) {
        if (document.querySelector("#" +socketID).classList.contains("isPinkWool")) {
-++storePoint
+++storePoint;
     }
    }
 break;
 case cursorClick.classList.contains("isRedWool"):
      if (document.querySelector("#refillWoolCreate" + (currentWoolGrabbed - 1)).classList.contains("isRedWool")) {
        if (document.querySelector("#" +socketID).classList.contains("isRedWool")) {
-++storePoint
+++storePoint;
     }
    }
 break;
@@ -316,7 +314,7 @@ function deleteWoolSockets() {
 }
 function specificHueRandomizer() {
     // 0 = yellow, 45 = green, 135 = blue, 200 = purple, 250 = pink, 295 = red
-let number = Math.floor(Math.random() * 6) // 0, 1, 2, 3, 4, 5
+let number = Math.floor(Math.random() * 6); // 0, 1, 2, 3, 4, 5
 let hueRandom = 0;
 switch (number) {
 case 0:
@@ -383,14 +381,9 @@ case 295:
 document.querySelector("#" + refillableWoolID).classList.add("isRedWool");
 break;
 }
-document.querySelector("#" + refillableWoolID).addEventListener("click", 
-function() {
-    woolGrab.play();
-    currentWoolGrabbed = i + 1;
-    showCursor(i, hueType); // tells showcursor which wool coord it is
-}
-);
-
+document.querySelector("#" + refillableWoolID).addEventListener("click", function() {
+    eeee(i, hueType);
+});
 }
 document.querySelector("#refillWoolCreate0").style.left = refilledWoolCoordX1 + "px";
 document.querySelector("#refillWoolCreate0").style.top = refilledWoolCoordY1 + "px";
@@ -400,8 +393,14 @@ document.querySelector("#refillWoolCreate2").style.left = refilledWoolCoordX3 + 
 document.querySelector("#refillWoolCreate2").style.top = refilledWoolCoordY3 + "px";
 }
 
+function eeee(e, h) {
+   woolGrab.play();
+    currentWoolGrabbed = e + 1;
+    showCursor(e, h); // tells showcursor which wool coord it is
+}
+
 function showCursor(clickedWoolid, clickedWoolHue) {
-    cursorClick.style.display = "block"
+    cursorClick.style.display = "block";
 switch (clickedWoolid) {
     case 0:
 cursorClick.style.left = refilledWoolCoordX1 + "px";
@@ -463,7 +462,7 @@ let endPosX = 0;
 if (windowWidth > 800) {
 endPosX = 850;
 } else {
-endPosX = 250;
+endPosX = 150;
 }
 let grabbingThisWool = null;
 switch (woolGrabbed) {
@@ -529,8 +528,11 @@ grabbingThisWool.style.left = starPosX + "px";
 
 setInterval(function() {//interval function for timer 
 if (gameHasStarted === true) {
+    let secondNumber = 0;
+    let forthNumber = 0;
+    let sixthNumber = 0;
     if (Points < 100) {
-    elapsedTime = elapsedTime + 1 // 00:00:00 means 10 milliseconds, pressing finish resets timer
+    elapsedTime = elapsedTime + 1; // 00:00:00 means 10 milliseconds, pressing finish resets timer
     secondNumber = Math.floor(elapsedTime / 6000); 
     secondsTimer = (elapsedTime / 100) % 60;
     forthNumber = Math.floor(secondsTimer);
@@ -571,7 +573,7 @@ part.style.display="none";
 }
 function resetNavBarState() {
     for(let part of allNavBar){ 
-part.style.color="#ffedfd"
+part.style.color="#ffedfd";
 }
 }
 function resetFeltingPageState() {
@@ -629,25 +631,25 @@ homeBtn.addEventListener("click", function () {
         resetNavBarState();
 showPage("#homePage"); // type page name here
 homeBtn.style.color="#835db9";
-})
+});
 feltBtn.addEventListener("click", function () {
       resetActivePages();
           resetNavBarState();
 showPage("#feltingPage"); // type page name here
 feltBtn.style.color="#835db9";
-})
+});
 guideBtn.addEventListener("click", function () {
       resetActivePages();
           resetNavBarState();
 showPage("#guidePage"); // type page name here
 guideBtn.style.color="#835db9";
-})
+});
 miniBtn.addEventListener("click", function () {
       resetActivePages();
       resetNavBarState();
 showPage("#minigamePage"); // type page name here
 miniBtn.style.color="#835db9";
-})
+});
 
 let qn = new Array(5); // makes a...new empty array of 5.
 quizBtn.addEventListener("click", function () {
@@ -656,12 +658,12 @@ for (let i=0; i < 5; i++) {
     let addi = i+1;
     let queryString = "input[name= 'q"+addi+"']:checked";
 qn[i]=document.querySelector(queryString).value;
-if(qn[i]=="true")scoreofQuiz++
+if(qn[i]=="true")scoreofQuiz++;
 }
 quizScore.innerHTML = "SCORE:" +scoreofQuiz;
 hasDoneQuiz = true;
 }
-})
+});
 for (let i = 0; i < 3; i++) {
 let selector = null;
 let imgSelector = null;
@@ -672,7 +674,7 @@ imgSelector = needleImage;
 break;
 case 1:
 selector = boardPage;
-imgSelector = boardImage
+imgSelector = boardImage;
 break;
 case 2:
 selector = woolPage;
@@ -680,71 +682,88 @@ imgSelector = feltImage;
 break;
 }
 imgSelector.addEventListener("click", function () {
+ohgod(i, selector, imgSelector);
+});
+}
+function ohgod(e, r, t) {
 resetFeltingIconState();
 resetFeltingPageState();
-showFeltPage(selector, imgSelector, i+1);
-})
+showFeltPage(r, t, e+1);
 }
+
 
 needleImage.addEventListener("mouseover", function () {
 if (feltPageOn !== 1) {
 needleImage.classList.add("hovered");
 }
-}) 
+});
 needleImage.addEventListener("mouseout", function () {
 needleImage.classList.remove("hovered");
-})
+});
 
 boardImage.addEventListener("mouseover", function () {
 if (feltPageOn !== 2) {
 boardImage.classList.add("hovered");
 }
-}) 
+}) ;
 boardImage.addEventListener("mouseout", function () {
 boardImage.classList.remove("hovered");
-})
+});
 
 feltImage.addEventListener("mouseover", function () {
 if (feltPageOn !== 3) {
 feltImage.classList.add("hovered");
 }
-}) 
+}) ;
 feltImage.addEventListener("mouseout", function () {
 feltImage.classList.remove("hovered");
-})
+});
 //image popup listeners, might want to make it a func inside a func
 // zindex layers: 0: base, 1: shadow filter, 2: elements, 3: images
 // note to self: convert to funcs in the afternoon ya
 //optimize eventlisteners since its very much repeated
-for (let i = 0; i < (amountOfHiddenImages); i++) { //starts from 1
-images[i].addEventListener("mouseover", function () {
+function validatorHellA(e) {
 if ((windowWidth > 800) && (isImagePopUpOpen === false)) {
- imageGrowOnHover(divImage[i], images[i]);
-images[i].classList.add("grow");
+ imageGrowOnHover(divImage[e], images[e]);
+images[e].classList.add("grow");
 }
-}) 
-
-images[i].addEventListener("mouseout", function () {
+}
+function validatorHellB(e) {
 if ((isHoveringOverPage === true) && (isImagePopUpOpen === false)) {
     // this is to check if user clicks off when in a image page or not
-    imageUnHover(divImage[i], images[i]);
-images[i].classList.remove("grow");
+    imageUnHover(divImage[e], images[e]);
+images[e].classList.remove("grow");
 }
-}) 
+}
+function validatorHellC(e) {
+        if (windowWidth > 800) {
+    imageOpenOnClick(hiddenText[e]);
+    images[e].style.display = "absolute";
+images[e].classList.remove("grow");
+    }
+}
+function validatorHellD(e) {
+    imageCloseOnClick(hiddenText[e]);
+images[e].classList.remove("grow");
+imageUnHover(divImage[e], images[e]);
+}
+
+for (let i = 0; i < (amountOfHiddenImages); i++) { //starts from 1
+images[i].addEventListener("mouseover", function () {
+validatorHellA(i);
+});
+
+images[i].addEventListener("mouseout", function () {
+validatorHellB(i);
+}) ;
 
 images[i].addEventListener("click", function () {
-    if (windowWidth > 800) {
-    imageOpenOnClick(hiddenText[i]);
-    images[i].style.display = "absolute";
-images[i].classList.remove("grow");
-    }
-})
+validatorHellC(i);
+});
 
 hiddenText[i].addEventListener("click", function () { // click off
-imageCloseOnClick(hiddenText[i]);
-images[i].classList.remove("grow");
-imageUnHover(divImage[i], images[i]);
-})
+validatorHellD(i);
+});
 
 }
 
@@ -752,32 +771,32 @@ imageUnHover(divImage[i], images[i]);
 // event handlers for play button etc
 spritePlay.addEventListener("mouseover", function () {
 spritePlay.classList.add("picked");
-}) 
+}) ;
 
 spritePlay.addEventListener("mouseout", function () {
 spritePlay.classList.remove("picked");
-}) 
+}) ;
 spritePlay.addEventListener("click", function () { // START THE GAME
 startGame();
-})
+});
 
 brightMode.addEventListener("click", function () { // click off
 currentMinigameTheme = 0;
 resetDayNightButtons();
 brightMode.classList.add("picked");
 playArea.style.filter = "none";
-})
+});
 darkMode.addEventListener("click", function () { // click off
 currentMinigameTheme = 1;
 resetDayNightButtons();
 darkMode.classList.add("picked");
-playArea.style.filter = "brightness(48%) contrast(132%) sepia(25%) hue-rotate(199deg)"
+playArea.style.filter = "brightness(48%) contrast(132%) sepia(25%) hue-rotate(199deg)";
 if (hasViewedWarning === false) {
 darkWarning.style.display = "block";
     hasViewedWarning = true;
     console.log(darkWarning.style.display);
 }
-})
+});
 darkWarning.addEventListener("click", function () { // click off
 switch (numberOfWarningClicks) {
     case 0:
@@ -790,14 +809,14 @@ break;
 numberOfWarningClicks = 2;
 break;
 }
-}) 
+}) ;
 
 finishButton.addEventListener("mouseover", function () { // click off
 finishButton.classList.add("picked");
-})
+});
 finishButton.addEventListener("mouseout", function () { // click off
 finishButton.classList.remove("picked");
-})
+});
 document.querySelector("#fakeSubmit").addEventListener("click", function () { 
     if (submittedEmail == false) {
 document.querySelector("#bField").value = "";
@@ -806,7 +825,7 @@ document.querySelector("#fakeSubmit").style.backgroundColor = "#73AD21";
 document.querySelector("#fakeSubmit").innerHTML = "Successful!";
 submittedEmail = true;
     }
-})
+});
 //
 finishButton.addEventListener("click", function () { // click off
 // reset all current wools, implement score grant later
@@ -832,8 +851,8 @@ if (windowWidth > 800) {
 document.querySelector("#darkwoolCreate" +i).style.left = "850px";
 document.querySelector("#whitewoolCreate" +i).style.left = "850px";
 } else {
-    document.querySelector("#darkwoolCreate" +i).style.left = "250px";
-document.querySelector("#whitewoolCreate" +i).style.left = "250px";
+    document.querySelector("#darkwoolCreate" +i).style.left = "150px";
+document.querySelector("#whitewoolCreate" +i).style.left = "150px";
 }
 }
 ++amtOfRounds;
@@ -850,7 +869,7 @@ scoreBox.innerHTML = Points +"!";
 Timer.innerHTML = amtOfRounds + "Taken";
 }
 }
-})
+});
 
 window.addEventListener("scroll", function(){ 
    var difference = window.pageYOffset || document.documentElement.scrollTop; 
